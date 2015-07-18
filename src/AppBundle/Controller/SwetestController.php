bec1a7532950e0eac176a0650fac00501cba5048
Tomasz Stąpor <tomaszstapor@gmail.com>
2015-07-18 22:54:50 +0200

    First impression, see: app_dev.php/swetest

--- src/AppBundle/Controller/SwetestController.php
+++ src/AppBundle/Controller/SwetestController.php
@@ -20,16 +20,16 @@ class SwetestController extends Controller
 
         // ephemeris of Mercury (-p2) starting on 1 Dec 1900,
         // 15 positions (-n15) in two-day steps (-s2)
-        $swetest->query('-b5.1.2002 -p -house12.05,49.50,k -ut12:30')->execute();
+//        $swetest->query('-b5.1.2002 -p -house12.05,49.50,k -ut12:30')->execute();
 
         // another query way
-        $arr = [
-            'b' => '5.1.2002',
-            'p' => null,
-            'house' => '12.05,49.50,k',
-            'ut' => '12:30',
-        ];
-        $swetest->query($arr)->execute();
+//        $arr = [
+//            'b' => '5.1.2002',
+//            'p' => null,
+//            'house' => '12.05,49.50,k',
+//            'ut' => '12:30',
+//        ];
+//        $swetest->query($arr)->execute();
 
         // angular distance of moon (-p1) from sun (-d0) for 10
         // consecutive days (-n10).
@@ -49,19 +49,19 @@ class SwetestController extends Controller
         // Midpoints between Saturn (-p6) and Chiron (-DD) for 100
         // consecutive steps (-n100) with 5-day steps (-s5) with
         // longitude in degree-sign format (-f..Z) rounded to minutes (-roundmin)
-        $swetest->query('-p6 -DD -b1.12.1900 -n100 -s5 -fPTZ -head -roundmin')->execute();
+//        $swetest->query('-p6 -DD -b1.12.1900 -n100 -s5 -fPTZ -head -roundmin')->execute();
 
         // another query way
-        $arr = [
-            'p' => 6,
-            'D' => 'D',
-            'b' => '1.12.1900',
-            'n' => 100,
-            's' => 5,
-            'f' => 'PTZ',
-            'roundmin',
-        ];
-        $swetest->query($arr)->execute();
+//        $arr = [
+//            'p' => 6,
+//            'D' => 'D',
+//            'b' => '1.12.1900',
+//            'n' => 100,
+//            's' => 5,
+//            'f' => 'PTZ',
+//            'roundmin',
+//        ];
+//        $swetest->query($arr)->execute();
 
 
         return array(
